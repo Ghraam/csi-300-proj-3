b0982957f2ded8616c660c7d1529a2b26e4e5b08
Graham Finlayson-Fife <graham@finlayson-fife.org>
2022-08-11 21:37:09 -0400

    add model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ApplicationRecord
+  has_many :retweets, through: :retweets, foreign_key: "retweeted"
+  has_many :retweeted_tweets, through: :retweets, foreign_key: "quote_tweet"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :followers, through: :follows, foreign_key: "followee"
+  has_many :following, through: :follows, foreign_key: "follower"
 end
