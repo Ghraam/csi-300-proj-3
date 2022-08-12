456c3e3fc62f27ae57fac7d67f436cb0d8c561ee
Graham Finlayson-Fife <graham@finlayson-fife.org>
2022-08-12 16:38:02 -0400

    user has_many tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,6 @@
 class Tweet < ApplicationRecord
+  belongs_to :user, foreign_key: "handle"
   has_many :retweets, through: :retweets, foreign_key: "retweeted"
   has_many :retweeted_tweets, through: :retweets, foreign_key: "quote_tweet"
+  has_many :likes, through: :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :followers, through: :follows, foreign_key: "followee"
   has_many :following, through: :follows, foreign_key: "follower"
+  has_many :tweets
 end
